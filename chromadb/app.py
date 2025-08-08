24cd4a0b00ff7e70dc3f713b09721cdfd25a4077
ykmia <yakimchatbotia@gmail.com>
2025-08-08 16:35:44 +0200

    Update app.py

--- chromadb/app.py
+++ chromadb/app.py
@@ -1,7 +1,11 @@
 import chromadb
 import chromadb.config
-from chromadb.server.fastapi import FastAPI
+from chromadb.server.fastapi import import FastAPI
 
 settings = chromadb.config.Settings()
 server = FastAPI(settings)
 app = server.app()
+
+@app.get("/")
+def root():
+    return {"status": "ok"}
